f58437426974123702ee1f3953e6f5bead21f3dd
sum1991 <ls.sumukh@gmail.com>
2017-10-06 22:25:17 -0600

    adding he driver function

--- sorted_element_rotated.js
+++ sorted_element_rotated.js
@@ -21,24 +21,34 @@ findingPivot(arr,low,high)
     return this.findingPivot(arr,mid+1,high)
 }
 binarySearch(arr,low,high,key){
-if(high<low)
-    return -1;
-let mid = Math.floor((high+low)/2);
-if(key == arr[mid])
-{
-    return mid;
+    if(high<low)
+        return -1;
+    let mid = Math.floor((high+low)/2);
+    if(key == arr[mid])
+    {
+        return mid;
+    }
+    if(key>arr[mid])
+    {
+        return this.binarySearch(arr,mid+1,high,key)
+    }
+    return this.binarySearch(arr,low,mid-1,key)
 }
-if(key>arr[mid])
-{
-    return this.binarySearch(arr,mid+1,high,key)
+pivotedBinarySearch(arr,low,high,key){
+
+    let pivot = this.findingPivot(arr,low,high)
+    if(pivot==-1)
+        this.binarySearch(arr,low,high,key)
+    if(arr[pivot]==key)
+        return pivot;
+    if(arr[low]<key)
+        return this.binarySearch(arr,low,pivot-1,key)
+    return this.binarySearch(arr,pivot-1,high,key)
 }
-return this.binarySearch(arr,low,mid-1,key)
-
-
-}
-
 
 }
 
 let rotate= new rotatedElementSearch();
-console.log(rotate.findingPivot([4,5,1,2,3],0,4))
+
+
+console.log(rotate.pivotedBinarySearch([4,5,1,2,3],0,4,3))
