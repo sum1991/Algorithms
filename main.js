742486bfe7a4e83bfcf3437f138cfe7b9250d04d
sum1991 <ls.sumukh@gmail.com>
2017-11-11 15:11:17 -0700

    pushing the valid parenthesis

--- main.js
+++ main.js
@@ -1,25 +1,31 @@
 var isValid = function(s) {
     let validate=true
     let charMap= new Map()
-    charMap.set("{","}")
-    charMap.set("[","]")
-    charMap.set("(",")")
     charMap.set("}","{")
     charMap.set("]","[")
     charMap.set(")","(")
+    let bracketArray=[]
     let i=0;
-    while(i<s.length){
-     
+    let lastIndex=s.length-1;
+    while(i<=lastIndex){
+        if(charMap.has(s[i]))
+        {
            let check =charMap.get(s[i]);
-            if(check==s[i+1]){
-                i=i+2;
+            if(check==(bracketArray.pop())){
+                i++;
             }
             else{
                 validate=false
                 break;
             }
-                
+        }
+        else{
+        bracketArray.push(s[i])
+        i++;
+        }       
     }
+    if(bracketArray.length>0 || s.length<=1)
+        validate=false;
     return validate
 };
-isValid("()")
+console.log(isValid("[[["))
